ecf2fe696c6b6ae6a19caa627c5167e9d9cc8c75
sosik <sosik@users.noreply.github.com>
2014-04-07 00:09:09 +0200

    Initial implementation of MongoDb realted modules
    Next implementation of MongoDB related stuff as updated
    mongoDriver helper and base userDao functionality

--- src/server/_mongoDriver.js
+++ src/server/_mongoDriver.js
@@ -1,5 +1,5 @@
-module.exports = function(MongoClient) {
-	var database;
+module.exports = function(MongoClient, ObjectID) {
+	var _database = null;
 
 	return {
 		/* async db initialization */
@@ -9,11 +9,47 @@ module.exports = function(MongoClient) {
 					callback(err);
 				}
 
-				database = db;
+				_database = db;
 				callback(null);
 			});
 		},
-			getDb: function() {
+		getDb: function() {
+			//TODO add logging
+			if (!_database) {
+				throw new Error('Database instance is not initialized!');
+			}
+
+			return _database;
+		},
+		close: function() {
+			_database.close();
+		},
+		/**
+		 * Function converts ObjectId form mongo returned object into flat
+		 * hexadecimal representation of id
+		 *
+		 * @param obj to mangle
+		 * @return mangled object
+		 */
+		_id2id: function(obj) {
+			if (obj && obj._id) {
+				var mangledId = obj._id.toHexString();
+				obj.id = mangledId;
+				delete obj._id;
+				return obj;
+			}
+			// nothing to mangle
+			return obj;
+		},
+		id2_id: function(obj) {
+			if (obj && obj.id) {
+				var idToMangle = obj.id;
+				obj._id = ObjectID.createFromHexString(idToMangle);
+				delete obj.id;
+				return obj;
+			}
+			// nothing to mangle
+			return obj;
 		}
 	};
 };
--- src/server/userDao.js
+++ src/server/userDao.js
@@ -1,3 +1,4 @@
+/** @module userDao */
 var mongoDriver = require('./mongoDriver');
 var extend = require('extend');
 
@@ -6,23 +7,61 @@ var DEFAULT_OPTIONS = {
 	collectionName: 'users'
 };
 
+/**
+ * @constructor
+ */
 var UserDao = function(options) {
 	var _options = extend(true, DEFAULT_OPTIONS, options || {});
-	var _userCollection = mongoDriver.getDb().collection(_options.collectionName || DEFAULT_USERS_COLLECTION_NAME);
+	var _userCollection = mongoDriver.getDb().collection(_options.collectionName);
 
-	this.saveUser = function(obj, callback) {
+	/**
+	 * @callback resultCallback
+	 * @param {Error} err - error or null
+	 * @param {Object} result - result of operation
+	 */
+
+	/**
+	 * Save user object
+	 *
+	 * @param {Object} obj - object to save
+	 * @param {resultCallback} callback - async callback
+	 */
+	this.save = function(obj, callback) {
 		_userCollection.save(obj, function(err, result) {
 			if (err) {
 				callback(err);
-			}
+			} 
 
+			mongoDriver._id2id(result);
 			callback(null, result);
 		});
 	};
-}
+
+	this.list = function(options, callback) {
+		_userCollection.find(function(err, cursor){
+			if (err) {
+				callback(err);
+			} else {
+				cursor.toArray(function(err, data) {
+					if (err) {
+						callback(err);
+					} else {
+						callback(null, data);
+					}
+				});
+			}
+		});
+	};
+
+	/**
+	 * @returns clone of actual options object 
+	 */
+	this.options = function() {
+		return extend({}, _options);
+	};
+};
 
 
 module.exports = {
-		UserDao: UserDao;
-	}
-}
+	UserDao: UserDao
+};
