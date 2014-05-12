e1320a4c856391937b248e5f2b8fe860a78757b5
Pavol Starek <pavol@starek.sk>
2014-05-12 09:17:08 +0200

    Fixed path to static content in server.js
    Server instance now correctly server static content.

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 var express = require('express');
+var path = require('path');
 
 var app = express();
-app.use(express.static(__dirname + '/client/html'));
+app.use(express.static(path.join(process.cwd(), 'build', 'client/html')));
 
 // TODO change behavior based on env property
 app.disable('view cache');
